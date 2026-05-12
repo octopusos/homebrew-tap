81b79f725f0dd275186dee98fa6377e3b11f5767
Ran <ran@seacowtech.com>
2026-05-13 01:45:59 +1000

    octoryn 2.0.3
    Co-authored-by: octoryn-release-bot <release-bot@octopusos.dev>

--- Formula/octoryn.rb
+++ Formula/octoryn.rb
@@ -1,28 +1,28 @@
 class Octoryn < Formula
   desc "Octoryn LLM official command-line interface"
   homepage "https://octopusos.dev"
-  version "0.1.3"
+  version "2.0.3"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v0.1.3/octoryn-darwin-arm64"
-      sha256 "0d9dd6b1aeee1afb84464ce1aa7d18e4524e5b76d191d7b53d871ef5cef21d81"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.3/octoryn-darwin-arm64"
+      sha256 "8bd68116ceeffe4d54f74d093e7ccf66a804eeccf5b70db284b2f3c0040702ed"
     end
     on_intel do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v0.1.3/octoryn-darwin-x64"
-      sha256 "a967c73d1c33b447a69f470fd8006584432046f40f07e51eba206c3ce79af2fd"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.3/octoryn-darwin-x64"
+      sha256 "f041d19f5a41994bac23ee07458086dafdd58717f6e11bb0e0d61c9ca0fd293e"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v0.1.3/octoryn-linux-arm64"
-      sha256 "ded5481b3b4dd122fc881727b2fdd7a3f2fb8a14e0b98e8eafaa6665b4738a1c"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.3/octoryn-linux-arm64"
+      sha256 "54307f96a00a190f32207d92c55d65262d8dccd86b2cff27a519cc8c0901a061"
     end
     on_intel do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v0.1.3/octoryn-linux-x64"
-      sha256 "be89ac41bec4da7ab6a6f83eca1b0929c6445ffeb9535563b3375e9e1a101ae5"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.3/octoryn-linux-x64"
+      sha256 "cbf44a661371714cdc0d35f78037697ba99b043841147f300abf9380a0176542"
     end
   end
 
