b12780ed26a0cadd27e799681acfb814742d4c4d
Ran <ran@seacowtech.com>
2026-05-14 23:54:29 +1000

    octoryn 2.0.5 (#6)
    Co-authored-by: octoryn-release-bot <release-bot@octopusos.dev>

--- Formula/octoryn.rb
+++ Formula/octoryn.rb
@@ -1,28 +1,28 @@
 class Octoryn < Formula
   desc "Octoryn LLM official command-line interface"
   homepage "https://octopusos.dev"
-  version "2.0.4"
+  version "2.0.5"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.4/octoryn-darwin-arm64"
-      sha256 "ab3eefc9a87b76664dcc2b56721de6e8a89a8db63da43b4461472e3eb2843b2f"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.5/octoryn-darwin-arm64"
+      sha256 "5b3d454b93f2ec3f470f26794f91fecc13d0a34059cb90ef01b557f7c19aebff"
     end
     on_intel do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.4/octoryn-darwin-x64"
-      sha256 "8460cb2573f8fdceeb8eae8d8c7ab619de525fac0127e35e2eed21c6e9385579"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.5/octoryn-darwin-x64"
+      sha256 "28d4eb18b318f70dd98adae02ff3728b5598bd86662b24b60c07fe73c87a9e0d"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.4/octoryn-linux-arm64"
-      sha256 "f1b74227c89cbc91f7ebb84961d9ce7b5c8afed0a8c6e765bcb9cd3df39399f2"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.5/octoryn-linux-arm64"
+      sha256 "1c00db12a160b23024f40dcf168d222ea3928d4d31ababb4ba28e436e27215b3"
     end
     on_intel do
-      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.4/octoryn-linux-x64"
-      sha256 "65701c7dd19bc0284281db320076cef4f952b093434cf13462eeea08e68d6d27"
+      url "https://github.com/octopusos/octoryn-cli-bin/releases/download/cli-v2.0.5/octoryn-linux-x64"
+      sha256 "2a12b2cdd7c9d7a7564b045129e2dd8bd74801a9b38747209975e1baf937dd95"
     end
   end
 
